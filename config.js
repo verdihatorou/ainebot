d7e27547983a902d657625dc84872b24cb945156
MAOU BOTZ <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-06-19 13:06:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
-  // [number, dia creator/owner?, dia developer?]
+  ['6285706735450'],
+  ['6285706735450'],
+  ['6283869803330', 'Owner', true]
+  // [number, dia creator, dia developer]
 ] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@XziroID'
+  var sticker_author = 'By.verdibotz'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '© XziroID'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
