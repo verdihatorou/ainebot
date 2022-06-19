b8b022c2b423ef66463cb1f878a6ff84bac16810
MAOU BOTZ <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-06-19 15:40:02 +0700

    Create verdi_ncs.js

---
+++ plugins/verdi_ncs.js
@@ -0,0 +1,17 @@
+//tqto.
+# tanks to 
+* ainechan
+*kangsadboy
+*DLL
+info? 
+wa.me//6285706735450
+FB:verdi kawai
+Github:github.com/XziroID 
+
+let fetch = require("node-fetch")
+Woriking run to web
+let false
+let false
+let version script/0.1
+let version /2.3.45
+consle= turu gaes
