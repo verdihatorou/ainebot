68874fbb1572e59e0df53d21c468f2c1a4a6bef1
MAOU BOTZ <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-06-19 14:23:20 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [085706735450]
+│ •  [085706735450]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
